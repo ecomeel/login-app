710b427cc1a18cd4e40f0bdc8c281a98e477e051
ecomeel <ecomeel@gmail.com>
2023-12-23 04:10:49 +0300

    login and registr routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import HomePage from './components/Homepage'
-import './App.css'
 import { Routes, Route } from 'react-router-dom'
+
+import './App.css'
+
+import HomePage from './components/Homepage'
+import LoginPage from './components/LoginPage'
+import RegistrPage from './components/RegistrPage'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/registration' element={<RegistrPage />} />
     </Routes>
   )
 }
